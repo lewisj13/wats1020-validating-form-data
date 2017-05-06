7a819242c9e5c9bcd45c8809e54c3d323d5e4be9
lewisj13 <lewisj13@seattleu.edu>
2017-05-05 21:18:05 -0700

    Updated index.html and main.js
    Updated the files to make the form work within the scope of the
    assignment.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,66 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+
+
+//
+  $(document).on('ready', function(){
+
+//Validation  and Requirements
+    $.validator.addMethod("valueNotEquals", function(value, element, arg){
+   return arg != value;
+  }, "Please select option from dropdown.");
+
+  $('#order-form').validate({
+    submitHandler: function(form) {
+      form.submit();
+    },
+    rules: {
+      "your-name": {
+        required: true,
+        maxlength: 128,
+        digits: false
+      },
+      "your-state": {
+        required: true,
+        maxlength: 2,
+        minlength: 2
+      },
+       "your-zip": {
+        required: true,
+        minlength: 5,
+        digits: true
+      },
+       "card-holder-name": {
+         required: true,
+         maxlength: 128,
+         digits: false
+       },
+       "card-number": {
+         required: true,
+         creditcard: true,
+       },
+       "expiry-month":{
+         required: true,
+         valueNotEquals: "Month",
+       },
+       "expiry-year": {
+         required: true,
+         valueNotEquals: "Year",
+       },
+       "cvv":{
+         required: true,
+         digits: true,
+         maxlength: 3,
+         minlength: 3
+       },
+       "shipping-method": {
+         required: true,
+         valueNotEquals: "Select One",
+       },
+       "comments": {
+         maxlength: 500,
+       },
+     },
+ });
+});
